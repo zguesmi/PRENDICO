b20c4b59cbe786fb2ec5d7d46fd618d241d1f332
Le-Caignec <robinlecaignec@yahoo.fr>
2023-11-17 23:35:04 +0300

    improve ui and add API

--- disaster-api/src/main.ts
+++ disaster-api/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS for all routes
+  app.use((_, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // Replace with your frontend origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
   await app.listen(8080);
 }
 bootstrap();
--- ui/src/pages/multiStepForm.tsx
+++ ui/src/pages/multiStepForm.tsx
@@ -8,14 +8,17 @@ import {
   Step,
   StepLabel,
   TextField,
+  CircularProgress,
 } from "@mui/material";
 
 type FormProps = {
   onNextStep: () => void;
+  userId: number;
 };
 
 export default function MultiStepForm() {
   const [step, setStep] = useState(0);
+  const userId = Math.floor(Math.random() * 1_000_000) + 1;
 
   const handleNextStep = () => {
     setStep((prevStep) => prevStep + 1);
@@ -24,11 +27,11 @@ export default function MultiStepForm() {
   const renderForm = () => {
     switch (step) {
       case 0:
-        return <FormOne onNextStep={handleNextStep} />;
+        return <FormOne onNextStep={handleNextStep} userId={userId} />;
       case 1:
-        return <FormTwo onNextStep={handleNextStep} />;
+        return <FormTwo onNextStep={handleNextStep} userId={userId} />;
       case 2:
-        return <FormThree onNextStep={handleNextStep} />;
+        return <FormThree onNextStep={handleNextStep} userId={userId} />;
       case 3:
         return <Success />;
       default:
@@ -40,102 +43,183 @@ export default function MultiStepForm() {
     <Box sx={{ p: 6, maxWidth: 900, margin: "auto" }}>
       <Stepper activeStep={step} alternativeLabel sx={{ p: 2 }}>
         <Step>
-          <StepLabel>Form 1</StepLabel>
+          <StepLabel>Verify Phone Number</StepLabel>
         </Step>
         <Step>
-          <StepLabel>Form 2</StepLabel>
+          <StepLabel>verify Geolocation</StepLabel>
         </Step>
         <Step>
-          <StepLabel>Form 3</StepLabel>
+          <StepLabel>Claim Compensation</StepLabel>
         </Step>
         <Step>
           <StepLabel>Success</StepLabel>
         </Step>
       </Stepper>
-      <Paper sx={{ p: 3, maxWidth: 700, margin: "auto" }}>{renderForm()}</Paper>
+      <Paper
+        sx={{
+          p: 3,
+          margin: "auto",
+          backgroundColor: "rgba(255, 255, 255, 0.4)",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {renderForm()}
+        </Box>
+      </Paper>
     </Box>
   );
 }
 
-const FormOne = ({ onNextStep }: FormProps) => {
+const FormOne = ({ onNextStep, userId }: FormProps) => {
   const [identityNumber, setIdentityNumber] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [haveVerificationCode, setHaveVerificationCode] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const handleNext = () => {
-    // You can add validation logic here before moving to the next step
-    onNextStep();
+  const handleNext = async () => {
+    setLoading(true);
+
+    const phoneNumber = identityNumber.trim();
+    const firstEndpoint = `http://localhost:8081/verificationcode?phoneNumber=${phoneNumber}`;
+
+    try {
+      const firstResponse = await fetch(firstEndpoint);
+
+      if (!firstResponse.ok) {
+        throw new Error(`HTTP error! Status: ${firstResponse.status}`);
+      }
+      const firstData = await firstResponse.json();
+      setHaveVerificationCode(true);
+      console.log("First Response:", firstData);
+    } catch (error) {
+      console.error("Error calling API:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSecondEndpoint = async () => {
+    const secondEndpoint = `http://localhost:8081/verifycode?verificationCode=${verificationCode}&userSessionId=${userId}&phoneNumber=${identityNumber.trim()}`;
+
+    try {
+      const secondResponse = await fetch(secondEndpoint);
+
+      if (!secondResponse.ok) {
+        throw new Error(`HTTP error! Status: ${secondResponse.status}`);
+      }
+
+      const secondData = await secondResponse.json();
+      console.log("Second Response:", secondData);
+
+      onNextStep(); // Move to the next step after calling the second endpoint
+    } catch (error) {
+      console.error("Error calling second API:", error);
+    }
   };
 
   return (
-    <div>
+    <Box sx={{ width: "100%", mx: 4 }}>
       <Typography variant="h6" gutterBottom>
-        Form 1
+        Fill it
       </Typography>
       <TextField
-        label="Identity Number"
+        label="Telephone Number"
         variant="outlined"
         fullWidth
         value={identityNumber}
         onChange={(e) => setIdentityNumber(e.target.value)}
         margin="normal"
       />
-      <Button variant="contained" onClick={handleNext} sx={{ mt: 2 }}>
-        Verify
-      </Button>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <div>
+          <Button
+            variant="contained"
+            onClick={handleNext}
+            disabled={!identityNumber.trim()} // Disable if verificationCode is empty
+          >
+            Get Verification Code
+          </Button>
+          {haveVerificationCode && (
+            <div>
+              <TextField
+                label="Verification Code"
+                variant="outlined"
+                fullWidth
+                value={verificationCode}
+                onChange={(e) => setVerificationCode(e.target.value)}
+                margin="normal"
+              />
+              <Button
+                variant="contained"
+                onClick={handleSecondEndpoint}
+                disabled={!verificationCode.trim()} // Disable if verificationCode is empty
+              >
+                Verify My Code
+              </Button>
+            </div>
+          )}
+        </div>
+      )}
+    </Box>
+  );
+};
+
+const FormTwo = ({ onNextStep, userId }: FormProps) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleNext = async () => {
+    setLoading(true);
+
+    const disasterEndpoint = `http://localhost:8080/disaster?userSessionId=${userId}`;
+
+    try {
+      const response = await fetch(disasterEndpoint);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      onNextStep();
+    } catch (error) {
+      console.error("Error calling API:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <Button variant="contained" onClick={handleNext}>
+          Prove My Geolocation
+        </Button>
+      )}
     </div>
   );
 };
 
-const FormTwo = ({ onNextStep }: FormProps) => {
-  const [fieldTwo, setFieldTwo] = useState("");
-
+const FormThree = ({ onNextStep, userId }: FormProps) => {
   const handleNext = () => {
-    // You can add validation logic here before moving to the next step
+    //TODO: Call smart contract
     onNextStep();
   };
 
   return (
     <div>
-      <Typography variant="h6" gutterBottom>
-        Form 2
-      </Typography>
-      <TextField
-        label="Field Two"
-        variant="outlined"
-        fullWidth
-        value={fieldTwo}
-        onChange={(e) => setFieldTwo(e.target.value)}
-        margin="normal"
-      />
       <Button variant="contained" onClick={handleNext} sx={{ mt: 2 }}>
-        Submit
-      </Button>
-    </div>
-  );
-};
-
-const FormThree = ({ onNextStep }: FormProps) => {
-  const [fieldThree, setFieldThree] = useState("");
-
-  const handleNext = () => {
-    // You can add validation logic here before moving to the next step
-    onNextStep();
-  };
-
-  return (
-    <div>
-      <Typography variant="h6" gutterBottom>
-        Form 3
-      </Typography>
-      <TextField
-        label="Field Three"
-        variant="outlined"
-        fullWidth
-        value={fieldThree}
-        onChange={(e) => setFieldThree(e.target.value)}
-        margin="normal"
-      />
-      <Button variant="contained" onClick={handleNext} sx={{ mt: 2 }}>
-        Claim
+        Claim Compensation
       </Button>
     </div>
   );
