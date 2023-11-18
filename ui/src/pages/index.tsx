c58796887f76e315fa35f432fd821bcccc86c8d5
Le-Caignec <robinlecaignec@yahoo.fr>
2023-11-18 14:49:20 +0300

    clean import

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import GradientBG from "../components/GradientBG.js";
 import {
   AppBar,
